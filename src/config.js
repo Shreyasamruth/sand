217c99fe8632fbf00cfc9b8232a8458deffc00f3
shreyas <shreyasamruth18@gmail.com>
2026-08-13 20:08:28 +0530

    Auto-heal: Updated live tunnel URL to https://institute-citizenship-ballet-member.trycloudflare.com

--- src/config.js
+++ src/config.js
@@ -6,7 +6,7 @@ export const appConfig = {
   releaseDate: "June 26, 2018",
   
   // PERMANENT HIGH-SPEED GOOGLE DRIVE STORAGE (RELIABLE)
-  downloadUrl: "https://resistant-decent-mistress-lanes.trycloudflare.com",
+  downloadUrl: "https://institute-citizenship-ballet-member.trycloudflare.com",
   
   // Alternate mirror links
   mirrors: [
